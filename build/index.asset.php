88bda5034d724a9e76d359bf03b3fbe8acddc146
Jonathan Bossenger <jonathanbossenger@gmail.com>
2020-05-22 10:32:59 +0200

    Version and build asset update
    Reordering Controls

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'f4b1cf4943b11d562c02e089da4966f0');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'fa8359129b99466ccba685a1ffe3da90');
